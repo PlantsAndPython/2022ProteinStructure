4704b719c4fba27bd03d1d17d6aa08ac3bbd8e51
James Lennon <lennonj1@msu.edu>
2022-11-21 17:47:13 -0500

    Save primary sequence to fasta

--- group2/extract_atoms.py
+++ group2/extract_atoms.py
@@ -1,35 +1,38 @@
 from os.path import join
 from pathlib import Path
 
-import mdtraj as mdj
 import numpy as np
 from glob import glob
 from joblib import Parallel, delayed
+from Bio import SeqIO
+from Bio.PDB.PDBParser import PDBParser
 
 def extract_coords_and_seq(pdb_path, output_dir):
-    traj = mdj.load_pdb(pdb_path)
-    top = traj.topology
-
-    # Load carbon alpha atoms of each chain
-    c_alpha_coords = {}
-    residues = {}
-    for chain in top.chains:
-        # 'CA' indicates the carbon alpha atoms
-        indices = top.select(f'chainid {chain.index} and name CA')
-
-        # Ignore chains that have none
-        if len(indices):
-            # MDTraj considers a PDB file to be a trajectory with one frame
-            coords = traj.xyz[0][indices]
-            c_alpha_coords[str(chain.index)] = coords
-
-        # Keep only the amino acid name (don't need the index)
-        chain_residues = [r.name[:3] for r in chain.residues]
-        residues[str(chain.index)] = chain_residues
-
     pdb_id = Path(pdb_path).stem
-    np.savez(join(output_dir, 'coordinates', f'{pdb_id}_c_alphas.npz'), **c_alpha_coords)
-    np.savez(join(output_dir, 'primary_seqs', f'{pdb_id}_residues.npz'), **residues)
+
+    seq_io = SeqIO.parse(pdb_path, 'pdb-seqres')
+    parser = PDBParser()
+    structure = parser.get_structure(pdb_id, pdb_path)
+
+    all_coords = {}
+    seqs = []
+    for model in structure.get_list():
+        for chain, record in zip(model.get_list(), seq_io):
+            chain_coords = []
+
+            for residue in chain.get_list():
+                try:
+                    chain_coords.append(residue['CA'].get_coord())
+                except KeyError:
+                    # No carbon alpha's were present in the amino acid
+                    pass
+
+            if chain_coords:
+                all_coords[str(model.get_id())] = {str(chain.get_id()):chain_coords}
+                seqs.append(record)
+
+    np.savez(join(output_dir, 'coordinates', f'{pdb_id}_c_alphas.npz'), **all_coords, )
+    SeqIO.write(seqs, join(output_dir, 'primary_seqs', f'{pdb_id}.fasta'), 'fasta')
 
     print(f'Saved {pdb_id}\'s c_alpha coordinates and primary sequence to file')
 
@@ -37,5 +40,12 @@ pdb_dir = join('Project', 'PDBs')
 pdb_paths = glob(join(pdb_dir, '*.pdb'))
 output_dir = join('Project', 'outputs')
 
-n_proc = min(len(pdb_paths), 20)
-Parallel(n_jobs=n_proc)(delayed(extract_coords_and_seq)(pdb_path, output_dir) for pdb_path in pdb_paths)
+TESTING = True
+
+if TESTING:
+    for i in range(20):
+        for pdb_path in pdb_paths:
+            extract_coords_and_seq(pdb_path, output_dir)
+else:
+    n_proc = min(len(pdb_paths), 20)
+    Parallel(n_jobs=n_proc)(delayed(extract_coords_and_seq)(pdb_path, output_dir) for pdb_path in pdb_paths)
